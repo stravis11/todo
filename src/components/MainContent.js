a7e4444d7cce784a5422d36793052140237d3ae8
stravis11 <stevetravis11@gmail.com>
2019-07-11 16:36:26 -0400

    Initial commit

---
+++ src/components/MainContent.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <div>
+      <input type="checkbox" />
+      <p>Text </p>
+      <input type="checkbox" />
+      <p>Text </p>
+      <input type="checkbox" />
+      <p>Text </p>
+    </div>
+  );
+}
+
+export default MainContent;
